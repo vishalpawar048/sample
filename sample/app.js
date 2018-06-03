b7553c00006209b61a4ca1cd433d0c57a58f5524
user <vishalpawar048@gmail.com>
2018-06-03 09:08:07 +0530

    server

--- sample/app.js
+++ sample/app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// var server = app.listen(app.get('port'), function () {
+//   console.log('server listening on port ' + server.address().port);
+// });
+
 module.exports = app;
